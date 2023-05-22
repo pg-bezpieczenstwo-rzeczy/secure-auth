de5ea125ee65005f28114813a91f16a6ed15a8b1
tpalinski <tpalinski@icloud.com>
2023-05-22 20:29:40 +0200

    Add final microservices

--- docker_run.sh
+++ docker_run.sh
@@ -14,6 +14,9 @@ java -jar ./src/WhoAsked-0.0.1-SNAPSHOT.jar &
 WHOASKED_PID=$!
 echo $WHOASKED_PID
 
+java -jar ./src/Babackend-0.0.1-SNAPSHOT.jar
+BACKEND_PID=$!
+echo $BACKEND_PID
 
 ROCKET_ADDRESS=127.0.0.1 ROCKET_PORT=8080 ./src/server
 TM_PID=$!
--- run_api.sh
+++ run_api.sh
@@ -16,6 +16,14 @@ java -jar ./src/WhoAsked-0.0.1-SNAPSHOT.jar &
 WHOASKED_PID=$!
 echo $WHOASKED_PID
 
+java -jar ./src/Babackend-0.0.1-SNAPSHOT.jar &
+BACKEND_PID=$!
+echo $BACKEND_PID
+
+java -jar ./src/Stressor-0.0.1-SNAPSHOT.jar &
+STRESSOR_PID=$!
+echo $STRESSOR_PID
+
 while [[ $string != 'exit' ]] || [[ $string == '' ]] # While string is different or empty...
 do
     read -p "Enter string: " string # Ask the user to enter a string
@@ -24,3 +32,5 @@ done
     kill -INT $SINVIS_PID
     kill $DESTRESSOR_PID
     kill $WHOASKED_PID
+    kill $BACKEND_PID
+    kill $STRESSOR_PID
